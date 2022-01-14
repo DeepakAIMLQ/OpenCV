fa27ed19cff9c7a5288b17fb7692ca60b51c9a5e
DeepakAIMLQ <74923940+DeepakAIMLQ@users.noreply.github.com>
2022-01-14 11:59:46 +0530

    saved fil

--- Started_with_Images.py
+++ Started_with_Images.py
@@ -1,2 +1,10 @@
 # Reading and Displaying Images in OPEN CV
+
 import cv2
+
+# Reading Image
+
+image = cv2.imread(filename=r'E:\OpenCV\OpenCV\images\DSC_2962.JPG')
+cv2.imshow('deepak marriage', image)
+cv2.waitKey(10000)
+cv2.destroyAllWindows()
